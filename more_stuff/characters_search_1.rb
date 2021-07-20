64b032d5c4dc2cbd9a5bd0771828d86e95b5c29e
Antony Lao <antony.lao@laposte.net>
2021-07-20 08:32:34 +0000

    more_stuff/character_search_1.rb : change method name (search_lab to search_chars)

--- more_stuff/characters_search_1.rb
+++ more_stuff/characters_search_1.rb
@@ -1,4 +1,4 @@
-def search_lab(word, char_search)
+def search_chars(word, char_search)
   regex_char_search = Regexp.new(char_search)
   if word =~ regex_char_search
     puts word
@@ -7,8 +7,8 @@ def search_lab(word, char_search)
   end
 end
 
-search_lab("laboratory",'lab')
-search_lab("experiment", 'lab')
-search_lab("Pans Labyrinth", /lab/)
-search_lab("elaborate", /lab/)
-search_lab("polar bear", 'bear ')
+search_chars("laboratory",'lab')
+search_chars("experiment", 'lab')
+search_chars("Pans Labyrinth", /lab/)
+search_chars("elaborate", /lab/)
+search_chars("polar bear", 'bear ')
